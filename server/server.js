f3230f35ea6ee6c71242ade169a3ce750e34829a
son-david <sondavid92@gmail.com>
2015-12-03 19:06:15 -0800

    added template for listener page

--- server/server.js
+++ server/server.js
@@ -43,6 +43,52 @@ app.get('/', function(req, res) {
   res.sendFile(path.resolve('client/index.html'));
 });
 
+
+//
+
+var Mustache = require('mustache');
+var fs = require('fs');
+var path = require('path');
+
+var template = fs.readFileSync(path.join(__dirname, './mustache/template.html'),'utf-8', function(err, data) {
+  if (err) console.log(err);
+});
+
+var person = {
+    firstName: "Kurtis",
+    lastName: "W",
+    blogURL: "info"
+};
+var person2 = {
+    firstName: "Ethan",
+    lastName: "GODT",
+    blogURL: "test 2"
+};
+var person3 = {
+    firstName: "nikolai",
+    lastName: "stobie",
+    blogURL: "more info"
+};
+
+var html = Mustache.render(template, person);
+var html2 = Mustache.render(template, person2);
+var html3 = Mustache.render(template, person3);
+
+
+app.get('/test1', function (req, res) {
+  res.send(html);
+});
+
+app.get('/test2', function (req, res) {
+  res.send(html2);
+});
+app.get('/test3', function (req, res) {
+  res.send(html3);
+});
+
+//
+
+
 app.listen(port, function(error) {
   if (error) {
     console.error(error);
