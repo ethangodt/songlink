15b89b0efa551ca3dacff3b5f9674e665df10f84
Nicholas Stobie <npstobie@gmail.com>
2015-12-05 16:55:39 -0800

    Refactors the object that each request to serach returns to match proper format

--- server/requests/plainTextSearch.js
+++ server/requests/plainTextSearch.js
@@ -1,57 +1,25 @@
 // var echobest = require('echo-best');
-var client = (require('ituner')());
-var spotify = require('spotify');
-
-// var key = process.env.ECHONEST_KEY;
-// var echo = echobest(key);
-
-
-// exports.echoSearch = function(req, res) {
-//   var opts = {
-//     combined: req.query.search,
-//     results: 10,
-//     // sort: 'song_hotttnesss-desc',
-//     bucket: 'song_hotttnesss'
-//     // rank_type: 'relevance'
-//   };
-  
-//   echo('song/search', opts, function(error, response) {
-//     if (error) {
-//       console.error(error);
-//     } else {
-//       console.log(response);
-//       res.send(response.songs);
-//     }
-//   });
-// }
+var itunes = (require('ituner')());
 
 exports.appleSearch = function(req, res) {
-  // if (text.slice(0,4) === "http") {
-  //   if (text.includes("itun.es")) {
-  //     var arr = text.split('=');
-  //     var itunesID = arr[arr.length - 1];
-  //     console.log(spotifyID);
-  //   } else if (text.includes("spotify")) {
-  //     var arr = text.split('/');
-  //     var spotifyID = arr[arr.length - 1];
-  //     spotify.lookup({ type: 'track', id: spotifyID}, function(err, data) {
-  //         if ( err ) {
-  //             console.log('Error occurred: ' + err);
-  //             return;
-  //         }
-  //         console.log(data);      
-  //     });
-  //   }
-  // } else if (text.slice(0,8) === "spotify:") {
-  //   var arr = text.split(':');
-  //   var spotifyID = arr[arr.length - 1];
-  //   console.log(spotifyID)
-  // } else {
   var text = req.query.search;
-  client.search(text, function (err, results) {
+  itunes.search(text, function (err, result) {
     if (err) {
       console.error(err);
     }
-   res.status(200).send(results)
+    var arr = result.results;
+    var songs = [];
+    for (var i=0; i<arr.length; i++) {
+      songs.push({
+        name: arr[i].trackName,
+        album_name: arr[i].collectionName,
+        artist: arr[i].artistName,
+        itunes_id: arr[i].trackId,
+        album_art: arr[i].artworkUrl100,
+        album_art_size: 1000
+      })
+    }
+    console.log(songs);
+    res.status(200).send(songs)
   });
 }
--- server/songBuilder/verifyLink.js
+++ server/songBuilder/verifyLink.js
@@ -1,25 +1,11 @@
-// require identify url
-
-/*
-
-{
-  Spotify: id,
-  YouTube: id, mvp?
-  iTunes: id or something
-}
-
-*/
-
 // make call to string parse/identifier module
 
 // call spotify module with the goods about song id to get song information like title
 // if the request returns positive then check the db or build out the rest of the data
 // if the request does not turn out ok then you should end response with an error
 // should each module come built in with some sort of integrity detection? yes....
-
-
-var spotify = require("");
-var itunes = require("");
+var spotify = require("../providerModules/spotify.js");
+var itunes = require("../providerModules/itunes.js");
 
 exports.checkService = function(url) {
   if (text.slice(0,4) === "http") {
@@ -36,25 +22,17 @@ exports.checkService = function(url) {
 exports.spotifyURL = function(url) {
   var arr = text.split('/');
   var spotifyID = arr[arr.length - 1];
-  return spotifyID;
+  spotify(spotifyID);
 };
 
 exports.spotifyURI = function(uri) {
     var arr = text.split(':');
     var spotifyID = arr[arr.length - 1];
-    return spotifyID;
+    spotify(spotifyID);
 };
 
 exports.itunesURL = function(url) {
   var arr = text.split('=');
   var itunesID = arr[arr.length - 1];
-  return itunesID;
+  itunes(itunesID);
 };
-
-  // spotify.lookup({ type: 'track', id: spotifyID}, function(err, data) {
-  //     if ( err ) {
-  //         console.log('Error occurred: ' + err);
-  //         return;
-  //     }
-  //     console.log(data);      
-  // });
