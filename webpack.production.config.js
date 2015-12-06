dc50d57f43a967475a1f75a68eb5560e1e95f5df
Kurt Weiberth <kurt.weiberth@gmail.com>
2015-12-06 15:18:11 -0800

    Update webpack config and npm scripts for dev and prod serving

---
+++ webpack.production.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var node_modules_dir = path.resolve(__dirname, 'node_modules');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client/client.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: [node_modules_dir],
+      loader: 'babel-loader'
+    }]
+  }
+};
