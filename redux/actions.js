e57c078563699d6a43fd278424b3afb9968dc7e5
Kurt Weiberth <kurt.weiberth@gmail.com>
2015-12-05 17:02:12 -0800

    Alphabetize actions functions

--- redux/actions.js
+++ redux/actions.js
@@ -7,6 +7,76 @@ export function addLink(link) {
   }
 }
 
+export function clearResults() {
+  return {
+    type: 'CLEAR_RESULTS'
+  }
+}
+
+export function createLink(song) {
+  return (dispatch, getState) => {
+    dispatch(toggleLoadingLink(true))
+    $.ajax({
+      url: 'create',
+      method: 'POST',
+      data: {
+        song: song
+      },
+      cache: false,
+      error: err => {
+        console.error(err)
+      },
+      success: res => {
+        console.log('got a response from post to /create')
+      }
+    })
+    setTimeout( () => {
+      dispatch(addLink(song))
+      dispatch(toggleLoadingLink(false))
+    }, 1000)
+  }
+}
+
+export function search(text) {
+  return (dispatch, getState) => {  
+    dispatch(toggleLoadingSearch(true))
+    $.ajax({
+      url: 'search',
+      data: {
+        search: text
+      },
+      cache: false,
+      error: err => {
+        console.error(err)
+      },
+      success: res => {
+        const songs = res.map(song => {
+          return {
+            name: song.name,
+            album_name: song.album_name,
+            artist: song.artist,
+            itunes_id: song.itunes_id,
+            album_art: song.album_art,
+            album_art_size: song.album_art_size
+          }
+        })
+        dispatch(toggleLoadingSearch(false))
+        dispatch(updateResults(songs))
+      }
+    })
+  }
+}
+
+export function submitPreference(preference) {
+  return dispatch => {
+    dispatch(toggleLoadingPreference(true))
+    setTimeout(() => {
+      dispatch(toggleLoadingPreference(false))
+      dispatch(updatePreference(preference))
+    }, 1300)
+  }
+}
+
 export function toggleLoadingLink(isLoading) {
   return {
     type: 'TOGGLE_LOADING_LINK',
@@ -39,54 +109,5 @@ export function updateResults(results) {
   return {
     type: 'UPDATE_RESULTS',
     results: results
-  }
-}
-
-export function clearResults() {
-  return {
-    type: 'CLEAR_RESULTS'
-  }
-}
-
-export function submitPreference(preference) {
-  return dispatch => {
-    dispatch(toggleLoadingPreference(true))
-    setTimeout(() => {
-      dispatch(toggleLoadingPreference(false))
-      dispatch(updatePreference(preference))
-    }, 1300)
-  }
-}
-
-export function search(text) {
-  return (dispatch, getState) => {  
-    dispatch(toggleLoadingSearch(true))
-    $.ajax({
-      url: 'search',
-      data: {
-        search: text
-      },
-      cache: false,
-      error: err => {
-        console.error(err);
-      },
-      success: res => {
-        const songs = res.results.map(song => {
-          return {title: song.trackName, artist: song.artistName, artwork: song.artworkUrl60}
-        });
-        dispatch(toggleLoadingSearch(false))
-        dispatch(updateResults(songs))
-      }
-    })
-  }
-}
-
-export function createLink(song) {
-  return (dispatch, getState) => {
-    dispatch(toggleLoadingLink(true))
-    setTimeout( () => {
-      dispatch(addLink(song))
-      dispatch(toggleLoadingLink(false))
-    }, 1000)
   }
 }
