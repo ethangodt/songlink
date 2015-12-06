8b0ce741ba99f462e8bff4e3674ab339e1e36316
Kurt Weiberth <kurt.weiberth@gmail.com>
2015-12-05 17:02:38 -0800

    Update Result component to handle correct song info

--- components/Result.js
+++ components/Result.js
@@ -3,14 +3,16 @@ import React from 'react'
 var Result = React.createClass({
 
   handleClick: function () {
-    this.props.actions.createLink(this.props.result.artwork);
+    this.props.actions.createLink(this.props.result);
     this.props.actions.clearResults();
   },
 
   render: function() {
     return (
       <li onClick={this.handleClick}>
-        {this.props.result.title} - {this.props.result.artist}
+        <div>{this.props.result.name}</div>
+        <div>{this.props.result.artist}</div>
+        <div>{this.props.result.album_name}</div>
         <img src={this.props.result.artwork} height='30px'></img>
       </li>
     )
