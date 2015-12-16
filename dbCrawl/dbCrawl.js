59ba4ddd0ab6303e7026a036cd9daac43f1839c9
son-david <sondavid92@gmail.com>
2015-12-16 12:47:12 -0800

    implemented dbCrawl.js that is called on npm run dbCrawl

--- dbCrawl/dbCrawl.js
+++ dbCrawl/dbCrawl.js
@@ -1,9 +1,17 @@
-var child_process = require('child_process');
-var child;
+require('shelljs/global');
 
-child = child_process.spawn("mongoexport --db songlink --collection songs --query "+
-        "'{ $or: [ {itunes_id: null}, {spotify_id: null}, {youtube_id: null} ] }' "+
-        "--out dbCrawlLog.csv --type=csv --fields 'hash_id,title,artist,album_title,youtube_id,spotify_id,itunes_id'"
-        );
+var arg = process.argv.slice(2);
 
-console.log(child);
+var commands = { //write custom commands here
+  findUndef : "'{ $or: [ {itunes_id: null}, {spotify_id: null}, {youtube_id: null} ] }'"
+}
+
+function crawl (arg) {
+
+  exec("mongoexport --db songlink --collection songs --query "+
+    arg + " --out dbCrawlLog.csv --type=csv --fields 'hash_id,title,artist,album_title,youtube_id,spotify_id,itunes_id'", {async:true});
+
+}
+if (commands[arg[0]]) {
+  crawl(commands[arg[0]]);
+}
