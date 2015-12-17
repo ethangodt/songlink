5630ec20c2ebacbb2f5c51f9a01fd408a8cedb20
son-david <sondavid92@gmail.com>
2015-12-16 17:04:05 -0800

    fixed redirect on saving preference

--- server/handlers/linkPage.js
+++ server/handlers/linkPage.js
@@ -47,8 +47,6 @@ function sendNonProvider (req, res, songFromDb) {
     }
   });
 
-  console.log('ssssss', songFromDb);
-
   var templateObj = {
     pageUrl : utils.makeSongLinkUrl(req.headers.host, songFromDb.hash_id),
     title : songFromDb.title,
