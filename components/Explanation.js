f8e43396cf3509a6a3d2ef5325940d0209ea4723
Ethan Godt <ethan@ethangodt.com>
2015-12-26 17:56:38 -0800

    Restyled search tips and added onload animation

--- components/Explanation.js
+++ components/Explanation.js
@@ -4,14 +4,41 @@ class Explanation extends Component {
 
   render() {
     return (
-      <div className="explanation wrapper">
-        <div className="primary"><span className="emphasis">search</span> by song or paste URL from Spotify or iTunes</div>
-        <div className="primary"><span className="emphasis">create</span> a unique songlink for your song</div>
-        <div className="primary"><span className="emphasis">share</span> this songlink with anyone</div>
+      <div className="explanationContainer">
+        <p>Create shareable songlinks from:</p>
+        <ul>
+          <li className="unloaded"><span className="fa fa-search"></span>Search</li>
+          <li className="unloaded"><span className="fa fa-spotify"></span>Spotify urls</li>
+          <li className="unloaded"><span className="fa fa-apple"></span>Apple Music urls</li>
+          <li className="unloaded"><span className="fa fa-youtube-play"></span>YouTube urls</li>
+        </ul>
       </div>
     )
   }
 
 }
+
+var init = function () {
+  var tips = document.querySelector('.explanationContainer').getElementsByClassName('unloaded');
+  tips = Array.prototype.slice.call(tips);
+  var className = 'unloaded';
+  var loadTip = function(tip, count){
+    if (tip.classList) {
+      tip.classList.remove(className);
+    } else {
+      tip.className = tip.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+
+    if (count < tips.length) {
+      count++;
+      setTimeout(function () {
+        loadTip(tips[count - 1], count);
+      }, 600);
+    }
+  };
+  loadTip(tips[0], 1);
+};
+
+window.onload = init;
 
 export default Explanation
