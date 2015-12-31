bc4d88de5c137fe2305fc597f78db3c8e4e5ec7b
Ethan Godt <ethan@ethangodt.com>
2015-12-31 10:39:57 -0800

    Added The Beatles support and other minor fixes to the YouTube search functions

--- server/providers/index.js
+++ server/providers/index.js
@@ -10,4 +10,4 @@ module.exports = {
   itunes: itunes,
   spotify: spotify,
   youtube: youtube
-}
+};
--- server/providers/youtube.js
+++ server/providers/youtube.js
@@ -57,7 +57,7 @@ function fetchSearchResults(song, query, queryType, callback) {
           console.error(err);
           song.results.youtube[queryType].results = [];
         } else {
-          song.results.youtube[queryType].results = vids.length ? vids.slice(0, 5) : [];
+          song.results.youtube[queryType].results = vids.length ? vids : [];
         }
 
         callback(null, song);
@@ -131,6 +131,11 @@ function getTopYoutubeResult(song) {
     for (var i = 0; i < queryTypes.length; i++) {
       var topResult = song.results.youtube[queryTypes[i]].results[0];
 
+      // this checks for queryType that might return nothing
+      if (!topResult) {
+        continue;
+      }
+
       // runs the sequence of checks specifically for topResults, and also checks that the vid is not album with isNotTwiceAsLong
       var someCheck = function (test) {
         var currentDuration = convertYoutubeDuration(topResult.contentDetails.duration);
@@ -146,29 +151,59 @@ function getTopYoutubeResult(song) {
   };
 
   var testAllResults = function () {
-    // check for vids with hashtag in channel and approxDuration
+    // 1. EXTRAORDINARY EXCEPTION: checks for 'The Beatles' as the channelTitle because they're huge
     for (var i = 0; i < queryTypes.length; i++) {
       var allResults = song.results.youtube[queryTypes[i]].results;
+
+      // this checks for queryType that might return nothing
+      if (!allResults.length) {
+        continue;
+      }
+
       for (var j = 0; j < allResults.length; j++) {
         var currentChannel = allResults[j].snippet.channelTitle;
-        var currentDuration = convertYoutubeDuration(allResults[j].contentDetails.duration);
+        if (currentChannel === 'The Beatles') {
+          return allResults[j];
+        }
+      }
+    }
+
+    // 2. check for vids with hashtag in channel and approxDuration
+    for (i = 0; i < queryTypes.length; i++) {
+      allResults = song.results.youtube[queryTypes[i]].results;
+
+      // this checks for queryType that might return nothing
+      if (!allResults.length) {
+        continue;
+      }
+
+      for (j = 0; j < allResults.length; j++) {
+        currentChannel = allResults[j].snippet.channelTitle;
+        currentDuration = convertYoutubeDuration(allResults[j].contentDetails.duration);
         if (heuristics.containsHashtag(currentChannel) && heuristics.isApproxDuration(currentDuration, song.track_length)) {
           return allResults[j];
         }
       }
     }
 
-    // check for vids with just approxDuration
-    for (var k = 0; k < queryTypes.length; k++) {
-      allResults = song.results.youtube[queryTypes[k]].results;
-      for (var l = 0; l < allResults.length; l++) {
-        currentDuration = convertYoutubeDuration(allResults[l].contentDetails.duration);
+    // 3. check for vids with just approxDuration
+    for (i = 0; i < queryTypes.length; i++) {
+      allResults = song.results.youtube[queryTypes[i]].results;
+
+      // this checks for queryType that might return nothing
+      if (!allResults.length) {
+        continue;
+      }
+
+      for (j = 0; j < allResults.length; j++) {
+        currentDuration = convertYoutubeDuration(allResults[j].contentDetails.duration);
         if (heuristics.isApproxDuration(currentDuration, song.track_length)) {
-          return allResults[l];
+          return allResults[j];
         }
       }
     }
 
+    // if everything fails
     return undefined;
   };
 
@@ -191,7 +226,7 @@ function makeLinkFromId(youtubeId) {
 }
 
 function search(queryString, callback) {
-  youtube.search(queryString, 10, function(err, res) {
+  youtube.search(queryString, 20, function(err, res) {
     if (err) {
       console.error(err);
       callback(new Error('Could not find any yt search results due to error'), null);
@@ -207,43 +242,3 @@ function search(queryString, callback) {
     }
   });
 }
-
-// function verify(song, vids, callback) {
-//   song.youtube_id = vids[0].id;
-//   callback(null, song);
-//   var results = [];
-//   var vevo = song.artist.split(' ').join('').toLowerCase() + 'vevo';
-//   for (var i = 0; i < vids.length; i++) {
-//     var vidDuration = convertYoutubeDuration(vids[i].contentDetails.duration);
-//     if (vids[i].snippet.channelTitle.toLowerCase() === vevo && vids[i].snippet.title.toLowerCase().includes(song.title)) {
-//       if ((Math.abs(vidDuration - song.track_length) / song.track_length) < 0.15) {
-//         results.push(vids[i]);
-//       }
-//     }
-//     if (vids[i].snippet.channelTitle.toLowerCase() === song.artist.toLowerCase()) {
-//       results.push(vids[i])
-//     }
-//     if (compareDurations(vidDuration, song.track_length)) {
-//       results.push(vids[i]);
-//     }
-//   }
-//   var bestMatch = verify2(results);
-//   bestMatch ? song.youtube_id = bestMatch.id : song.youtube_id = vids[0].id;
-//   return callback(null, song);
-// }
-
-// function verify2(results) {
-//   var best = 0;
-//   var index;
-//   for (var i=0; i<results.length; i++) {
-//     if (results[i].statistics.likeCount > 100) {
-//       var stats = results[i].statistics;
-//       var rating = (stats.likeCount / stats.dislikeCount) * stats.viewCount;
-//       if (rating > best) {
-//         best = rating;
-//         index = i
-//       }
-//     }
-//   }
-//   return (results[index])
-// }
